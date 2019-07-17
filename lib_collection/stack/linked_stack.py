c4b4b3284ca3f30b349e7d5dfcc46a79925463f5
Xue Jiao <jiao.xue@shopee.com>
2019-07-17 18:16:31 +0800

    init linked stack

---
+++ lib_collection/stack/linked_stack.py
@@ -0,0 +1,28 @@
+class LinkedStack(object):
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __contains__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    __repr__ = __str__
+
+    def push(self, i):
+        pass
+
+    def pop(self):
+        pass
+
+    @property
+    def top(self):
+        pass
