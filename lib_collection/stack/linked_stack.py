f170565238260dc084b7621dd201123edf6ebca9
Xue Jiao <jiao.xue@shopee.com>
2019-07-17 18:26:53 +0800

    Implement Node for LinkedStack

--- lib_collection/stack/linked_stack.py
+++ lib_collection/stack/linked_stack.py
@@ -1,10 +1,30 @@
+class Node(object):
+    def __init__(self, v):
+        self.v = v
+        self.next = None
+
+    def __str__(self):
+        """
+        >>> n = Node('a')
+        >>> n
+        Node('a')
+        >>> n = Node(1)
+        >>> n
+        Node(1)
+        """
+        return 'Node({})'.format(repr(self.v))
+
+    __repr__ = __str__
+
+
 class LinkedStack(object):
 
     def __init__(self):
-        pass
+        self.n = 0
+        self.head = Node(None)
 
     def __len__(self):
-        pass
+        return self.n
 
     def __contains__(self):
         pass
@@ -18,7 +38,14 @@ class LinkedStack(object):
     __repr__ = __str__
 
     def push(self, i):
-        pass
+        """
+        >>> s = LinkedStack()
+        >>> s.push('a')
+        """
+        n = Node(i)
+        n.next = self.head.next
+        self.head.next = n
+        self.n += 1
 
     def pop(self):
         pass
