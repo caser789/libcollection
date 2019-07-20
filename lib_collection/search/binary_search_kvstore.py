624c6518dddc75348df5a7a10d1daa19a62fbb42
Xue Jiao <jiao.xue@shopee.com>
2019-07-20 13:53:53 +0800

    Implement get_index for BinarySearchKVStore

--- lib_collection/search/binary_search_kvstore.py
+++ lib_collection/search/binary_search_kvstore.py
@@ -1,10 +1,19 @@
+class NoneNode(object):
+    pass
+
+
+none = NoneNode()
+
 class BinarySearchKVStore(object):
 
     def __init__(self, capacity=2):
-        pass
+        self.n = 0
+        self.keys = [none] * capacity
+        self.values = [none] * capacity
+        self.capacity = capacity
 
     def __len__(self):
-        return 0
+        return self.n
 
     def __contains__(self, key):
         return False
@@ -18,7 +27,7 @@ class BinarySearchKVStore(object):
         pass
 
     def __setitem__(self, key, value):
-        pass
+        i = self.get_index(key)
 
     def __getitem__(self, key):
         pass
@@ -30,4 +39,31 @@ class BinarySearchKVStore(object):
         pass
 
     def del_min(self):
+        pass
+
+    def get_index(self, key):
+        """
+        >>> d = BinarySearchKVStore()
+        >>> d.n = 3
+        >>> d.keys = [1, 2, 3]
+        >>> d.get_index(2)
+        1
+        >>> d.get_index(1.5)
+        1
+        >>> d.get_index(2.5)
+        2
+        """
+        lo = 0
+        hi = self.n - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if key == self.keys[mid]:
+                return mid
+            elif key < self.keys[mid]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
+
+    def _resize(self, capacity):
         pass
