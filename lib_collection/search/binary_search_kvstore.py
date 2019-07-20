c6a4d537571f3b301c4a838e1f40e8a8cc3ff917
Xue Jiao <jiao.xue@shopee.com>
2019-07-20 13:36:07 +0800

    Init BinarySearchKVStore

---
+++ lib_collection/search/binary_search_kvstore.py
@@ -0,0 +1,33 @@
+class BinarySearchKVStore(object):
+
+    def __init__(self, capacity=2):
+        pass
+
+    def __len__(self):
+        return 0
+
+    def __contains__(self, key):
+        return False
+
+    @property
+    def min(self):
+        pass
+
+    @property
+    def max(self):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def del_max(self):
+        pass
+
+    def del_min(self):
+        pass
