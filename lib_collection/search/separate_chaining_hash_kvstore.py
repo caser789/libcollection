a789916157cd95d6d4a34ecf5aef5268e3a297b5
Xue Jiao <jiao.xue@shopee.com>
2019-07-22 22:01:26 +0800

    Init __iter__ and items for SeparateChainingHashKVStore

---
+++ lib_collection/search/separate_chaining_hash_kvstore.py
@@ -0,0 +1,38 @@
+from lib_collection.search.sequential_search_kvstore import SequentialSearchKVStore
+
+
+class SeparateChainingHashKVStore(object):
+
+    __item_cnt_per_bucket__ = 10
+
+    def __init__(self):
+        self.n = 0
+        self.bucket_cnt = 4
+        self.lst = [SequentialSearchKVStore() for _ in range(self.bucket_cnt)]
+
+    def __len__(self):
+        return self.n
+
+    def __iter__(self):
+        for kvstore in self.lst:
+            for k in kvstore:
+                yield k
+
+    def items(self):
+        for kvstore in self.lst:
+            for k, v in kvstore.items():
+                yield k, v
+
+    def _hash(self, k):
+        """
+        >>> d = SeparateChainingHashKVStore()
+        >>> d._hash('a')
+        1
+        >>> d._hash('b')
+        2
+        >>> d._hash('c')
+        3
+        >>> d._hash('d')
+        0
+        """
+        return sum(ord(i) for i in str(k)) % self.bucket_cnt
--- lib_collection/search/separate_chaining_hash_map.py
+++
@@ -1,6 +0,0 @@
-from lib_collection.search.sequential_search_kvstore import SequentialSearchKVStore
-
-
-class SeparateChainingHashMap(object):
-
-    __item_cnt_per_bucket__ = 10
