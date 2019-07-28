899f405f8d7e3c0e5824c6e1ea4cddb3566fbca5
Xue Jiao <jiao.xue@shopee.com>
2019-07-28 11:45:05 +0800

    Implement __repr__ for Node in bst_kvstore

---
+++ lib_collection/search/bst_kvstore.py
@@ -0,0 +1,53 @@
+class Node(object):
+    def __init__(self, k, v):
+        self.k = k
+        self.v = v
+        self.left = None
+        self.right = None
+        self.size = 1
+
+    def __repr__(self):
+        """
+        >>> n = Node(1, 'a')
+        >>> n
+        Node(1, 'a')
+        """
+        return 'Node({}, {})'.format(repr(self.k), repr(self.v))
+
+
+class BSTKVStore(object):
+
+    def __init__(self):
+        self.root = None
+        self.n = 0
+
+    def __setitem__(self, k, v):
+        """
+        >>> s = BSTKVStore()
+        >>> s[1] = 'a'
+        >>> s.root
+        Node(1, 'a')
+        """
+        self.root = self._put_to_node(self.root, k, v)
+
+    def __len__(self):
+        return self._get_size(self.root)
+
+    def _put_to_node(self, node, k, v):
+        if node is None:
+            return Node(k, v)
+
+        if node.k == k:
+            node.v = v
+        elif k < node.k:
+            node.left = self._put_to_node(node.left, k, v)
+        else:
+            node.right = self._put_to_node(node.right, k, v)
+
+        node.size = 1 + self._get_size(node.left) + self._get_size(node.right)
+        return node
+
+    def _get_size(self, node):
+        if node is None:
+            return 0
+        return node.size
