f7cc8c0667695b35f75b16505136f879f3c67ed5
Xue Jiao <jiao.xue@shopee.com>
2019-07-31 22:21:07 +0800

    Implement QuickUnionUnionFind.is_connected

--- lib_collection/union_find/quick_union_union_find.py
+++ lib_collection/union_find/quick_union_union_find.py
@@ -22,4 +22,12 @@ class QuickUnionUnionFind(object):
         return p
 
     def is_connected(self, p, q):
-        pass
+        """
+        >>> uf = QuickUnionUnionFind(5)
+        >>> uf.parents = [0, 1, 1, 2, 4]
+        >>> uf.is_connected(1, 3)
+        True
+        >>> uf.is_connected(1, 4)
+        False
+        """
+        return self.find(p) == self.find(q)
