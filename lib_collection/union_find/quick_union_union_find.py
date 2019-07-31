277299bc77847567fcf00d9b2a6d9093bfab75fa
Xue Jiao <jiao.xue@shopee.com>
2019-07-31 22:16:07 +0800

    Init QuickUnionUnionFind

---
+++ lib_collection/union_find/quick_union_union_find.py
@@ -0,0 +1,14 @@
+class QuickUnionUnionFind(object):
+
+    def __init__(self, n):
+        self.n = n
+        self.parents = range(n)
+
+    def union(self, p, q):
+        pass
+
+    def find(self, p):
+        pass
+
+    def is_connected(self, p, q):
+        pass
