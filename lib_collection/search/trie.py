3c4500f242a7000ac99d3686dbebb4edd4690085
Xue Jiao <jiao.xue@shopee.com>
2019-11-09 15:07:04 +0800

    Implement delitem for trie

--- lib_collection/search/trie.py
+++ lib_collection/search/trie.py
@@ -9,6 +9,7 @@ class Trie(object):
 
     def __init__(self):
         self.root = TrieNode(None)
+        self.n = 0
 
     def __setitem__(self, key, val):
         if val is None:
@@ -47,17 +48,39 @@ class Trie(object):
         return self._getnode(node.children[j], key, i+1)
 
     def __delitem__(self, key):
-        pass
+        self.root = self._delnode(self.root, key, 0)
 
-    def _delitem(self):
-        pass
+    def _delnode(self, node, key, i):
+        if node is None:
+            return
+
+        if i == len(key):
+            if node.val is not None:
+                self.n -= 1
+            node.val = None
+        else:
+            j = ord(key[i]) - ord('a')
+            node.children[j] = self._delnode(node.children[j], key, i+1)
+
+        if node.val is not None:
+            return node
+
+        for i in range(len(node.children)):
+            if node.children[i] is not None:
+                return node
 
     def __len__(self):
-        pass
+        return self.n
 
     def __contains__(self, key):
         node = self._getnode(self.root, key, 0)
         return node is not None
 
     def __iter__(self):
+        pass
+
+    def keys(self):
+        return self.keys_with_prefix("")
+
+    def keys_with_prefix(self, prefix):
         pass
