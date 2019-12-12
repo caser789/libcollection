3b08f691cf8d0c6d0318814618393d66d4a54e7e
Xue Jiao <jiao.xue@shopee.com>
2019-12-12 11:26:24 +0800

    Implemented _set_node for Trie

--- lib_collection/search/trie_kvstore.py
+++ lib_collection/search/trie_kvstore.py
@@ -9,6 +9,7 @@ class Trie(object):
     def __init__(self, capacity=26):
         self.capacity = capacity
         self.root = None
+        self.n = 0
 
     def __getitem__(self, key):
         """
@@ -26,7 +27,7 @@ class Trie(object):
         if node is not None:
             return node.val
 
-    def _get_node(self, node, key, level):
+    def _get_node(self, node, key, index):
         """
         >>> t = Trie()
         >>> n = t._get_node(t.root, 'a', 1)
@@ -49,9 +50,37 @@ class Trie(object):
         if node is None:
             return
 
-        if level == len(key):
+        if index == len(key):
             return node
 
-        c = key[level]
+        c = key[index]
         i = ord(c) - ord('a')
-        return self._get_node(node.children[i], key, level+1)
+        return self._get_node(node.children[i], key, index+1)
+
+    def __setitem__(self, key, val):
+        self.root = self._set_node(self.root, key, val, 0)
+
+    def _set_node(self, node, key, val, index):
+        """
+        >>> t = Trie()
+        >>> node = t._set_node(t.root, 'ab', 12, 2)
+        >>> node.val == 12
+        True
+        >>> t = Trie()
+        >>> node = t._set_node(t.root, 'ab', 12, 0)
+        >>> node.children[0].children[1].val == 12
+        True
+        """
+        if node is None:
+            node = Node(None)
+
+        if index == len(key):
+            if node.val is None:
+                self.n += 1
+            node.val = val
+            return node
+
+        c = key[index]
+        i = ord(c) - ord('a')
+        node.children[i] = self._set_node(node.children[i], key, val, index+1)
+        return node
