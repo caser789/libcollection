c7c017844e56913073dca3bf439425f0401f96e9
Xue Jiao <jiao.xue@seamoney.com>
2022-01-12 16:46:51 +0800

    undirected_graph_path

---
+++ lib_collection/graph/undirected_graph_path.py
@@ -0,0 +1,98 @@
+class Node(object):
+    def __init__(self, v):
+        self.next = None
+        self.v = v
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.root = None
+        self.n = 0
+
+    def put(self, v):
+        node = Node(v)
+        node.next = self.root
+        self.root = node
+        self.n += 1
+
+    def __len__(self):
+        return self.n
+
+    def __iter__(self):
+        h = self.root
+        while h:
+            yield h.v
+            h = h.next
+
+
+class UndirectedGraph(object):
+    def __init__(self, v):
+        self.v = v
+        self.e = 0
+        self.adjs = [LinkedList() for _ in range(v)]
+
+    def add_edge(self, v, w):
+        self.adjs[v].put(w)
+        self.adjs[w].put(v)
+        self.e += 1
+
+    def get_adjs(self, v):
+        for e in self.adjs[v]:
+            yield e
+
+    def get_degree(self, v):
+        return len(self.adjs[v])
+
+    def pprint(self):
+        print("{} vertices, {} edges".format(self.v, self.e))
+        for i, adjs in enumerate(self.adjs):
+            print("{}:".format(i), end="")
+            for e in adjs:
+                print(" {}".format(e), end="")
+            print("")
+
+
+class PathFinder(object):
+    def __init__(self, graph, s):
+        self.s = s
+        self.marked = [False for _ in range(graph.v)]
+        self.parents = [v for v in range(graph.v)]
+        self._dfs(graph, s)
+
+    def _dfs(self, graph, s):
+        self.marked[s] = True
+        for w in graph.get_adjs(s):
+            if self.marked[w]:
+                continue
+            self.parents[w] = s
+            self._dfs(graph, w)
+
+    def has_path_to(self, w):
+        return self.marked[w]
+
+    def path_to(self, w):
+        if not self.marked[w]:
+            return
+        res = []
+        while w != self.s:
+            res.append(w)
+            w = self.parents[w]
+        res.append(w)
+        return res
+
+
+if __name__ == '__main__':
+    g = UndirectedGraph(6)
+    g.add_edge(0, 5)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(2, 4)
+    g.add_edge(2, 3)
+    g.add_edge(1, 2)
+    g.add_edge(3, 4)
+    g.add_edge(3, 5)
+    g.pprint()
+
+    p = PathFinder(g, 0)
+    for v in range(g.v):
+        print(p.path_to(v))
